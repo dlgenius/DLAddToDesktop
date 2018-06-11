ce69710c99af3f6d5f99634162bbc8fff3c6ac47
donglei <dongleic@yonyou.com>
2018-06-11 14:17:35 +0800

    解决ios11停留在本地地址不跳转问题

--- DLAddToDesktop/DLAddToDesktop/AppDelegate.m
+++ DLAddToDesktop/DLAddToDesktop/AppDelegate.m
@@ -21,6 +21,10 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
+    [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
+        
+    }];
+
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
@@ -41,5 +45,6 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
 
 @end
